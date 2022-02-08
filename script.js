f2453f1e60ce6e3f3f98c2712eb65f8ec1faebec
Gustavo <gutog2105@gmail.com>
2022-02-08 16:19:37 -0300

    Correção de codigo

--- script.js
+++ script.js
@@ -1,90 +1,89 @@
-let porcentagem = ''
 
+class DadosCalc {
+	constructor(valorPedido, valorGorjeta, qtdPessoas) {
 
-function button(tipo, valor) {
-	if (tipo === 'btn') {
-		if (valor === '5') {
-			porcentagem = valor
-			console.log(porcentagem)
-			document.getElementById('5').classList.add('btn_escuro')
-			document.getElementById('10').classList.remove('btn_escuro')
-			document.getElementById('15').classList.remove('btn_escuro')
-			document.getElementById('25').classList.remove('btn_escuro')
-		} else if (valor === '10') {
-			porcentagem = valor
-			console.log(porcentagem)
-			document.getElementById('5').classList.remove('btn_escuro')
-			document.getElementById('10').classList.add('btn_escuro')
-			document.getElementById('15').classList.remove('btn_escuro')
-			document.getElementById('25').classList.remove('btn_escuro')
-		} else if (valor === '15') {
-			porcentagem = valor
-			console.log(porcentagem)
-			document.getElementById('5').classList.remove('btn_escuro')
-			document.getElementById('10').classList.remove('btn_escuro')
-			document.getElementById('15').classList.add('btn_escuro')
-			document.getElementById('25').classList.remove('btn_escuro')
-		} else if (valor === '25') {
-			porcentagem = valor
-			console.log(porcentagem)
-			document.getElementById('5').classList.remove('btn_escuro')
-			document.getElementById('10').classList.remove('btn_escuro')
-			document.getElementById('15').classList.remove('btn_escuro')
-			document.getElementById('25').classList.add('btn_escuro')
-		}		
-	} else if (tipo === 'text'){
-		porcentagem = valor
-		console.log(porcentagem)
-		document.getElementById('5').classList.remove('btn_escuro')
-		document.getElementById('10').classList.remove('btn_escuro')
-		document.getElementById('15').classList.remove('btn_escuro')
-		document.getElementById('25').classList.remove('btn_escuro')
+		this.valorPedido = valorPedido;
+		this.valorGorjeta = valorGorjeta;
+		this.qtdPessoas = qtdPessoas;
+		
+	}
+	validaCampos(){
+		for (const i in this) {
+			if (this[i] == undefined || this[i] == '' || this[i] == null) {
+				return false;	
+			}
+		}
+		return true;
+	}
+	resultado(){
+		let valorTotal = (this.valorPedido * this.valorGorjeta) / 100;
+		let valorPorPessoa = valorTotal / this.qtdPessoas;
+
+		let gorjeta = document.getElementById('gorjeta').value = valorTotal.toFixed(2);
+		let gorjetaPorPessoa = document.getElementById('gorjetaPorPessoas').value = valorPorPessoa.toFixed(2);
+	}
+}
+function calcular(){
+	const valorPedido = document.getElementById('valor_pedido');
+	const valorGorjeta = document.getElementById('porcentagem');
+	const qtdPessoas = document.getElementById('quantidade_pessoas');
+	const elementos = [valorPedido, valorGorjeta, qtdPessoas]
+	
+	let dadosCalc = new DadosCalc(valorPedido.value, valorGorjeta.value, qtdPessoas.value);
+	
+	if(!dadosCalc.validaCampos()){
+		for(const i in elementos){
+			if(elementos[i].value == '' || elementos[i].value == undefined || elementos[i].value == null) {
+				elementos[i].classList.add('border_red')
+			}
+		}
+	} else {
+		dadosCalc.resultado();
+	}
+}
+function remove(elemento){
+	if(elemento == 1){
+		document.getElementById('valor_pedido').classList.remove("border_red");
+	}
+	if(elemento == 2){
+		document.getElementById('quantidade_pessoas').classList.remove("border_red");
 	}	
 }
+function button(porcentagem) {
+	let button5 = document.getElementById('5').classList.contains("btn_escuro");
+	let button10 = document.getElementById('10').classList.contains("btn_escuro");
+	let button15 = document.getElementById('15').classList.contains("btn_escuro");
+	let button25 = document.getElementById('25').classList.contains("btn_escuro");
+	let porcento = document.getElementById('porcentagem').classList.contains("border_red");
 
-button()
-
-function calcular() {
-	let pedido = document.getElementById('valor_pedido');
-	let pessoas = document.getElementById('quantidade_pessoas');
-
-	if (pedido.value === '') {
-		pedido.className +=  ' border_red';
-	} else {
-		pedido.classList.remove('border_red')
+	if(button5){
+		document.getElementById('5').classList.remove('btn_escuro');
+	}
+	if(button10){
+		document.getElementById('10').classList.remove('btn_escuro');
+	}
+	if(button15){
+		document.getElementById('15').classList.remove('btn_escuro');
+	}
+	if(button25){
+		document.getElementById('25').classList.remove('btn_escuro');
 	}
 
-	if (pessoas.value === '') {
-		pessoas.className += ' icone_person_red border_red';
-	} else {
-		pessoas.classList.remove('border_red')
-		pessoas.classList.remove('icone_red')
+	if(porcentagem == 5){
+		document.getElementById('5').classList.add('btn_escuro');
+		document.getElementById('porcentagem').value = "5";
+	} else if(porcentagem == 10) {
+		document.getElementById('10').classList.add('btn_escuro');
+		document.getElementById('porcentagem').value = "10";
+	}else if(porcentagem == 15) {
+		document.getElementById('15').classList.add('btn_escuro');
+		document.getElementById('porcentagem').value = "15";
+	}else if(porcentagem == 25) {
+		document.getElementById('25').classList.add('btn_escuro');
+		document.getElementById('porcentagem').value = "25";
 	}
 
-	if (porcentagem === '' ){
-		document.getElementById('porcentagem').className += ' border_red'
-	} else {
-		document.getElementById('porcentagem').classList.remove('border_red')
-	}
-
-	resultado(pedido, porcentagem, pessoas)
-}
-
-function resultado(valor, porcento, pessoa) {
-
-
-	let pedido = parseInt(valor.value)
-	let porcentagem = parseInt(porcento)
-	let pessoas = parseInt(pessoa.value)
-
-	let gorjeta = pedido * porcentagem / 100
-	let gorjetaPessoa = gorjeta / pessoas
-
-	if (gorjeta >= 0 && gorjetaPessoa >= 0) {
-		document.getElementById('gorjeta').innerHTML = gorjeta
-		document.getElementById('gorjetaPorPessoas').innerHTML = gorjetaPessoa
-	} else {
-		document.getElementById('gorjeta').innerHTML = ''
-		document.getElementById('gorjetaPorPessoas').innerHTML = ''
+	if(porcento){
+		document.getElementById('porcentagem').classList.remove("border_red");
 	}
 }
